d339a7d6b7a0ebcf8f3858971f0a54f49ddd5e39
commony0 <nawoony98@gmail.com>
2023-11-15 11:08:56 +0900

    여행관련 sql 제작

---
+++ src/main/webapp/WEB-INF/doc/travel/travelcate.sql
@@ -0,0 +1,27 @@
+/* Table name: TRAVELCATE */
+DROP TABLE TRAVELCATE;
+
+CREATE TABLE TRAVELCATE(
+    CATENO       NUMBER(10)      NOT NULL,
+    NAME    VARCHAR2(30)      NOT NULL,   
+    SEQNO      NUMBER(7)      NOT NULL,   
+    VISIBLE    CHAR(1)    NOT NULL,
+    PRIMARY KEY (CATENO)
+);
+
+COMMENT ON TABLE TRAVELCATE is '여행 카테고리';
+COMMENT ON COLUMN TRAVELCATE.CATENO is '여행 카테고리 번호';
+COMMENT ON COLUMN TRAVELCATE.NAME is '카테고리 이름';
+COMMENT ON COLUMN TRAVELCATE.SEQNO is '출력 순서';
+COMMENT ON COLUMN TRAVELCATE.VISIBLE is '출력 모드';
+
+DROP SEQUENCE TRAVELCATE_SEQ;
+
+CREATE SEQUENCE TRAVELCATE_SEQ
+  START WITH 1              -- 시작 번호
+  INCREMENT BY 1            -- 증가값
+  MAXVALUE 99999            -- 최대값: 99999 --> NUMBER(5) 대응
+  CACHE 2                   -- 2번은 메모리에서만 계산
+  NOCYCLE;                  -- 다시 1부터 생성되는 것을 방지
+
+
