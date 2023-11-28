9e99b4c2388983a0f70f6b1796391a7901efd64f
hyeonsu6 <bhshs0508@naver.com>
2023-11-28 12:33:32 +0900

    erd 수정

--- src/main/java/dev/mvc/festivalcate/FestivalcateProc.java
+++ src/main/java/dev/mvc/festivalcate/FestivalcateProc.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 //Controller가 사용하는 이름 
 @Component("dev.mvc.festivalcate.FestivalcateProc")
 public class FestivalcateProc implements FestivalcateProcInter {
-	@Autowired // FestivalcateDAOInter interface를 구현한 클래스의 객체를 만들어 자동으로 할당해라.
+	@Autowired // FestivalfestivalcateDAOInter interface를 구현한 클래스의 객체를 만들어 자동으로 할당해라.
 	private FestivalcateDAOInter festivalcateDAO;
 
 	@Override
@@ -22,6 +22,56 @@ public class FestivalcateProc implements FestivalcateProcInter {
 	public ArrayList<FestivalcateVO> list_all() {
 		ArrayList<FestivalcateVO> list = this.festivalcateDAO.list_all();
 
+		return list;
+	}
+
+	@Override
+	public FestivalcateVO read(int festivalcateno) {
+		FestivalcateVO festivalcateVO = this.festivalcateDAO.read(festivalcateno);
+		return festivalcateVO;
+	}
+
+	@Override
+	public int update(FestivalcateVO festivalVO) {
+		int cnt = this.festivalcateDAO.update(festivalVO);
+
+		return cnt;
+	}
+
+	@Override
+	public int delete(int festivalfestivalcateno) {
+		int cnt = this.festivalcateDAO.delete(festivalfestivalcateno);
+
+		return cnt;
+	}
+
+	@Override
+	public int update_seqno_forward(int festivalcateno) {
+		int cnt = this.festivalcateDAO.update_seqno_forward(festivalcateno);
+		return cnt;
+	}
+
+	@Override
+	public int update_seqno_backward(int festivalcateno) {
+		int cnt = this.festivalcateDAO.update_seqno_backward(festivalcateno);
+		return cnt;
+	}
+
+	@Override
+	public int update_visible_y(int festivalcateno) {
+		int cnt = this.festivalcateDAO.update_visible_y(festivalcateno);
+		return cnt;
+	}
+
+	@Override
+	public int update_visible_n(int festivalcateno) {
+		int cnt = this.festivalcateDAO.update_visible_n(festivalcateno);
+		return cnt;
+	}
+
+	@Override
+	public ArrayList<FestivalcateVO> list_all_y() {
+		ArrayList<FestivalcateVO> list = this.festivalcateDAO.list_all_y();
 		return list;
 	}
 
