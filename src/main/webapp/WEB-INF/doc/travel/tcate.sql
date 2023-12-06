00a8e29dec58a41b24c3e050c1dda762a4e76559
commony0 <nawoony98@gmail.com>
2023-12-06 11:12:23 +0900

    erd 수정

--- src/main/webapp/WEB-INF/doc/travel/tcate.sql
+++ src/main/webapp/WEB-INF/doc/travel/tcate.sql
@@ -4,8 +4,8 @@ DROP TABLE TCATE;
 CREATE TABLE TCATE(
     TCATENO       NUMBER(10)      NOT NULL,
     NAME    VARCHAR2(30)      NOT NULL,   
-    SEQNO      NUMBER(7)      NOT NULL,   
-    VISIBLE    CHAR(1)    NOT NULL,
+    SEQNO      NUMBER(7)     DEFAULT 1   NOT NULL,   
+    VISIBLE    CHAR(1)   DEFAULT 'N'     NOT NULL,
     PRIMARY KEY (TCATENO)
 );
 
@@ -24,4 +24,10 @@ CREATE SEQUENCE TCATE_SEQ
   CACHE 2                   -- 2번은 메모리에서만 계산
   NOCYCLE;                  -- 다시 1부터 생성되는 것을 방지
 
-
+-- CREATE
+INSERT INTO tcate(tcateno, name) VALUES(tcate_seq.nextval, '먹거리');
+INSERT INTO tcate(tcateno, name) VALUES(tcate_seq.nextval, '해변휴양');
+INSERT INTO tcate(tcateno, name) VALUES(tcate_seq.nextval, '도심');
+INSERT INTO tcate(tcateno, name) VALUES(tcate_seq.nextval, '문화');
+INSERT INTO tcate(tcateno, name) VALUES(tcate_seq.nextval, '레저');
+INSERT INTO tcate(tcateno, name) VALUES(tcate_seq.nextval, '캠핑');
